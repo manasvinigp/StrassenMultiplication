55ca05ea47a06734f9a3031645b83d77d219636b
Manasvini G P <manasvinigp@gmail.com>
2024-07-13 16:28:57 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ import './index.css';
 import App from './App';
 
 // Function to initialize and render the React app
-const renderReactApp = () => {
-  const rootElement = document.getElementById('root');
-  ReactDOM.createRoot(rootElement).render(
+const rootElement = document.getElementById('root');
+
+// Function to initialize and render the React app
+ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  );
-};
+);
 
-// Export the function to global scope to be called from the static index.html
-window.renderReactApp = renderReactApp;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
