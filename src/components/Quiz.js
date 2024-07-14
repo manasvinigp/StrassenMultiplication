e4d49a8f443441f5f997b9980fd2344c25f5fed6
Manasvini G P <manasvinigp@gmail.com>
2024-07-14 21:58:53 +0530

    style modifications

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -10,16 +10,16 @@ const Quiz = () => {
             <div className="container">
                 <div className="row vh-100 align-items-center justify-content-center">
                     <div className="col-lg-8">
-                        <div className="card p-4" style={{ background: '#ebe8e8', borderColor: '#373333' }}>
+                        <div className="card p-4" style={{ background: '#dcd8d8', borderColor: '#373333' }}>
                             <div className="d-flex justify-content-between gap-md-3">
                                 <h5 className='mb-2 fs-normal lh-base'>{question?.question}</h5>
-                                <h5 style={{ color: '#60d600', width: '100px', textAlign: 'right' }}>{quizs.indexOf(question) + 1} / {quizs?.length}</h5>
+                                <h5 style={{ color: '4B9F00', width: '100px', textAlign: 'right' }}>{quizs.indexOf(question) + 1} / {quizs?.length}</h5>
                             </div>
                             <div>
                                 {
                                     question?.options?.map((item, index) => <button
                                         key={index}
-                                        className={`option w-100 text-start btn text-white py-2 px-3 mt-3 rounded btn-dark ${correctAnswer === item && 'bg-success'}`}
+                                        className={`option w-100 text-start btn text-black py-2 px-3 mt-3 rounded btn-dark ${correctAnswer === item && 'bg-success'}`}
                                         onClick={(event) => checkAnswer(event, item)}
                                     >
                                         {item}
